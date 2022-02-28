0e58d8b645cab2d1f88003d085c73b6154f40924
Joel Swarns <jswarns@uniteddx.com>
2022-02-28 15:45:25 -0500

    Added activities for the place options

--- app/src/main/java/com/example/placestogo/MainActivity.java
+++ app/src/main/java/com/example/placestogo/MainActivity.java
@@ -1,14 +1,24 @@
 package com.example.placestogo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.mainAppBar);
+
     }
+
+
 }
---
+++ app/src/main/java/com/example/placestogo/MovieTheatersActivity.java
@@ -0,0 +1,14 @@
+package com.example.placestogo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MovieTheatersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_theaters);
+    }
+}
